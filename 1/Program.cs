991b8ae1a8217bb5b3b5c660d3f05cdae39568f8
morskibg <dimityrp@yahoo.com>
2017-09-28 21:05:27 +0200

    this gets 80 points (there is one wrong answer from JUDGE) https://judge.softuni.bg/Contests/Practice/Index/428#4

--- 1/Program.cs
+++ 1/Program.cs
@@ -10,20 +10,59 @@ namespace _1
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>();
-            int currentListIdx = 0;
             int sequenceToAdd = int.Parse(Console.ReadLine());
-            for(int i = 0; i < sequenceToAdd; ++i)
+            
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            for (int i = 0; i < sequenceToAdd - 1; ++i)
             {
-                if(i == 0)
-                {
-                    List<int> tempList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-                    numbers = tempList;
-                    continue;
-                }
                 int[] currentSequence = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-                
-               
+                List<int> increasedSeq = new List<int>();
+                bool isWronSequenceAppear = false;
+
+                increasedSeq.Add(currentSequence[0]);
+
+
+                for (int j = 0; j < currentSequence.Length - 1; ++j)
+                {
+                    if (currentSequence[j] > currentSequence[j + 1])
+                    {
+                        if (j < currentSequence.Length - 1)
+                        {
+                            isWronSequenceAppear = true;
+                        }
+                        break;
+                    }
+                    increasedSeq.Add(currentSequence[j + 1]);
+                }
+
+                while (true)
+                {
+                    int smalestNum = increasedSeq[0];
+                    int smallIndex = numbers.FindIndex(x => x >= smalestNum);
+                    if (smallIndex == -1)
+                    {
+                        numbers.Add(smalestNum);
+                    }
+                    else
+                    {
+                        numbers.Insert(smallIndex, smalestNum);
+                    }
+                    if (increasedSeq.Count > 0)
+                    {
+                        increasedSeq.RemoveAt(0);
+                        if (increasedSeq.Count == 0)
+                        {
+                            if (isWronSequenceAppear && smalestNum != numbers.Last())
+                            {
+                                int indexToRemove = numbers.FindIndex(x => x > smalestNum);
+                                int lengthToRemove = numbers.Count - indexToRemove;
+                                numbers.RemoveRange(indexToRemove, lengthToRemove);
+                            }
+                            break;
+                        }
+                    }
+                }
             }
             Console.WriteLine(string.Join(" ", numbers));
         }

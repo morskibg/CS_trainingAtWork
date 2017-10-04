ec9cca4d14fbdb486e3b5b1cce07fe1a98e27971
morskibg <dimityrp@yahoo.com>
2017-10-04 16:56:44 +0200

    +

--- NE_1/Program.cs
+++ NE_1/Program.cs
@@ -11,56 +11,62 @@ namespace NE_1
         static void Main()
         {
             int n = int.Parse(Console.ReadLine());
-            Dictionary<string, Dictionary<string, int>> clothDatabase = 
+            Dictionary<string, Dictionary<string, int>> clothDatabase =
                 new Dictionary<string, Dictionary<string, int>>();
-            
-            for(int i = 0; i < n; ++i)
+
+            for (int i = 0; i < n; ++i)
             {
-                char[] delimiters = " ->,".ToCharArray();
-               
+                string[] separator = new string[] {" ", "->", "," };
                 string[] args = Console.ReadLine()
-                    .Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+                    .Split(separator, StringSplitOptions.RemoveEmptyEntries)
                     .ToArray();
                 string color = args[0];
-                if(!clothDatabase.ContainsKey(color))
+                if (!clothDatabase.ContainsKey(color))
                 {
                     clothDatabase[color] = new Dictionary<string, int>();
-                    for(int j = 1; j < args.Length; ++j)
-                    {
-                        clothDatabase[color].Add(args[j], 1);                        
-                    }                    
-                }
-                else
-                {
                     for (int j = 1; j < args.Length; ++j)
                     {
-                        if (clothDatabase[color].ContainsKey(args[j]))
-                        {
-                            clothDatabase[color][args[j]]++;
-                        }
+                        clothDatabase[color].Add(args[j], 0);
                     }
                 }
+                   for (int j = 1; j < args.Length; ++j)
+                    {
+                        if (!clothDatabase[color].ContainsKey(args[j]))
+                        {
+                            clothDatabase[color][args[j]] = 0;
+                        }
+                    }
+                
+
+                for (int j = 1; j < args.Length; ++j)
+                {
+                    if (clothDatabase[color].ContainsKey(args[j]))
+                    {
+                        clothDatabase[color][args[j]]++;
+                    }
+                }
+
             }
-            bool isFound = false;
+            
             string[] toFind = Console.ReadLine().Split(' ').ToArray();
             string colorToFind = toFind[0];
             string itemTofind = toFind[1];
-            foreach(var color in clothDatabase.Keys)
+            foreach (var color in clothDatabase.Keys)
             {
                 Console.WriteLine($"{color} clothes:");
-                foreach(var item in clothDatabase[color])
+                foreach (var item in clothDatabase[color])
                 {
                     string founded = "";
-                    
-                    if(item.Key == itemTofind && color == colorToFind)
+
+                    if (item.Key == itemTofind && color == colorToFind)
                     {
-                        //isFound = true;
+                        
                         founded = " (found!)";
                     }
                     Console.WriteLine($"* {item.Key} - {item.Value}{founded}");
                 }
             }
-            
+
         }
     }
 }

e880689aa7a539a0b0725da12bbbeabb6883c1a7
morskibg <dimityrp@yahoo.com>
2017-10-04 16:27:22 +0300

    +

---
+++ Nested_D_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_D_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> uniqueNames = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; ++i)
+            {
+                uniqueNames.Add(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join("\n",uniqueNames));
+        }
+    }
+}
---
+++ Nested_D_4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_D_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedDictionary<string, SortedDictionary<string, SortedSet<string>>> uniqueGeoDatabase
+                = new SortedDictionary<string, SortedDictionary<string, SortedSet<string>>>();
+            for (int i = 0; i < n; ++i)
+            {
+                string[] args = Console.ReadLine().Split(' ').ToArray();
+                if (!uniqueGeoDatabase.ContainsKey(args[0]))
+                {
+                    uniqueGeoDatabase[args[0]] = new SortedDictionary<string, SortedSet<string>>
+                    {
+                        { args[1], new SortedSet<string>
+                            {args[2] }
+                        }
+                    };
+                }
+                if (!uniqueGeoDatabase[args[0]].ContainsKey(args[1]))
+                {
+                    uniqueGeoDatabase[args[0]][args[1]] = new SortedSet<string>
+                        {
+                            {args[2]}
+                        };
+                }
+                else
+                {
+                    uniqueGeoDatabase[args[0]][args[1]].Add(args[2]);
+                }
+            }
+            foreach(var continent in uniqueGeoDatabase)
+            {
+                Console.WriteLine($"{continent.Key}: ");
+                foreach(var country in continent.Value)
+                {
+                    Console.WriteLine($"  {country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}

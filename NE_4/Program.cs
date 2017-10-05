8cb48f0674ae8b792d59ca3896c0de229429ddf0
morskibg <dimityrp@yahoo.com>
2017-10-05 16:21:49 +0300

    +

---
+++ NE_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NE_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, HashSet<int>> shelsDatabase = new Dictionary<string, HashSet<int>>();
+            string input = Console.ReadLine();
+            while (input != "Aggregate")
+            {
+                string[] args = input.Split(' ');
+                string region = args[0];
+                int shellSize = int.Parse(args[1]);
+                if (!shelsDatabase.ContainsKey(region))
+                {
+                    shelsDatabase[region] = new HashSet<int>();
+                }
+                shelsDatabase[region].Add(shellSize);
+                input = Console.ReadLine();
+            }
+            foreach( var currRegion in shelsDatabase)
+            {
+                double shelsAverage = currRegion.Value.Average();
+                double shelsSum = currRegion.Value.Sum();
+                double giantShell = shelsSum - shelsAverage;
+                Console.WriteLine($"{currRegion.Key} -> {string.Join(", ", currRegion.Value)} ({Math.Ceiling(giantShell)})");    
+            }
+        }
+    }
+}

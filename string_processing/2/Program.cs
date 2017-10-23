11b0cd3b075384cf8e7408beebc23350e8174b0b
morskibg <dimityrp@yahoo.com>
2017-10-23 17:42:57 +0300

    +

---
+++ string_processing/2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            string matchStr = Console.ReadLine().ToLower();
+            int matchCounter = 0;
+            int lastIdx = input.LastIndexOf(matchStr);
+            while (lastIdx != -1)
+            {
+                ++matchCounter;                
+                lastIdx = input.LastIndexOf(matchStr, lastIdx + 1);
+            }
+            Console.WriteLine(matchCounter);
+        }
+    }
+}

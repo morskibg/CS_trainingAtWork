02b3693bcbda024d36d2949c7d2d86dbdd349043
morskibg <dimityrp@yahoo.com>
2017-11-06 23:06:36 +0200

    +

---
+++ exam_home/exam05_11_home/2. Anonymous Threat/2. Anonymous Threat.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Anonymous_Threat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringArr = Console.ReadLine().Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToList();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "3:1")
+                {
+                    break;
+                }
+                var query = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToArray();
+                string command = query[0];
+                if (command == "merge")
+                {
+                    int startIdx = int.Parse(query[1]);
+                    int endIdx = int.Parse(query[2]);
+                    if (startIdx < 0)
+                    {
+                        startIdx = 0;
+                    }
+                    if (endIdx > stringArr.Count - 1)
+                    {
+                        endIdx = stringArr.Count - 1;
+                    }
+                    if (startIdx >= endIdx)
+                    {
+                        continue;
+                    }
+                    StringBuilder merged = new StringBuilder();
+                    foreach (var currString in stringArr.Where((x, i) => i >= startIdx && i <= endIdx))
+                    {
+                        merged.Append(currString);
+                    }
+                    stringArr.RemoveRange(startIdx, endIdx - startIdx + 1);
+                    stringArr.Insert(startIdx, merged.ToString());
+                }
+                else if (command == "divide")
+                {
+                    int splitIdx = int.Parse(query[1]);
+                    int partitions = int.Parse(query[2]);
+                    string itemToSplit = stringArr[splitIdx];
+                    string[] splited = new string[partitions];
+                    int partitionSize = itemToSplit.Length / partitions;
+                    int subStringIdx = 0;
+                    for (int i = 0; i < partitions; ++i)
+                    {
+                        if (i == partitions - 1)
+                        {
+                            splited[i] = itemToSplit.Substring(subStringIdx);
+                        }
+                        else
+                        {
+                            splited[i] = itemToSplit.Substring(subStringIdx, partitionSize);
+                            subStringIdx += partitionSize;
+                        }
+                    }
+                    stringArr.RemoveAt(splitIdx);
+                    stringArr.InsertRange(splitIdx, splited);
+                }
+            }
+            Console.WriteLine(string.Join(" ", stringArr));
+        }
+    }
+}
---
+++ exam_home/exam05_11_home/3. Anonymous Vox/3. Anonymous Vox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03.Anonymous_Vox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encrypted = Console.ReadLine();
+            string placeHolders = Console.ReadLine();
+            string placeholderPattern = @"(?<start>[A-Za-z]+)(?<placeholder>.+)(?<end>\k<start>)";
+
+
+            var toReplace = Regex.Matches(encrypted, @"(?<start>[A-Za-z]+)(?<placeholder>.*)(?<end>\k<start>)")
+                .Cast<Match>()
+                .Select(x => x.Groups["placeholder"])
+                .ToList();
+            var placeholdersRaw = Regex.Matches(placeHolders, @"[\{](?<value>.*?)[\}]")
+                .Cast<Match>()
+                .Select(x => x.Groups["value"])
+                .ToList();
+          
+            int smallerSize = Math.Min(toReplace.Count, placeholdersRaw.Count);
+            
+            for (int i = 0; i < smallerSize; i++)
+            {
+                int occurence = encrypted.IndexOf(toReplace[i].ToString());
+                int sizeToRemove = toReplace[i].Length;
+                StringBuilder sb = new StringBuilder();
+                sb.Append(encrypted.Substring(0, occurence));
+                sb.Append(placeholdersRaw[i].ToString());
+                sb.Append(encrypted.Substring(occurence + sizeToRemove));
+                encrypted = sb.ToString();
+            }
+            Console.WriteLine(string.Join(" ", encrypted));
+
+        }
+    }
+}
